b88432ec1541857d40d2a5e6a38394a7a42a35f2
Михаил <letiagin.mixail@yandex.ru>
2017-03-15 14:25:29 +0300

    123

---
+++ RivgRequest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bad4iz
+ * Date: 14.03.2017
+ * Time: 19:39
+ */
+namespace Rivg;
+class RivgRequest
+{
+    protected $_url;
+    private $_a;
+    public function __construct($url)
+    {
+        $this->_url = $url;
+    }
+    public function createCurl($url = 'nul')
+    {
+        if($url != 'nul'){
+            $this->_url = $url;
+        }
+        $ch = curl_init($this->_url);
+        $_a = curl_exec($ch);
+        curl_close($ch);
+        return $_a;
+    }
+}
+
+$rivg = new RivgRequest("http://rivg-proj-1/request.php?Rivg");
+$resp = $rivg->createCurl();
+echo $resp;
